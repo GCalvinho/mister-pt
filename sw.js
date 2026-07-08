1116af8508ec5c33345f8080c881ee639d00e81a
Gonçalo <goncalo@local>
2026-07-08 06:14:15 +0000

    v37: passes rasteiros por defeito (aéreo só bolas longas/flutuadas) + origem variável das repetições

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="mister-pt-v36";
+const C="mister-pt-v37";
 self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./"])).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
